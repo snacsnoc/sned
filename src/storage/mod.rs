0c025aef939d56c821d922841d6c2f8f2cb101e2
Easton Elliott <easton@geekness.eu>
2026-05-26 03:36:03 -0700

    Remove dead storage re-exports
    - Remove GlobalFileNames, cleanup_orphaned_temp_files (unused)
    - Remove all migration re-exports except plan_dry_run_migration (all unused)
    - Remove SecretsStore, StateManager (unused)
    - Remove list_tasks, sort_by_timestamp, total_pages (unused)
    - Remove TaskStorage (unused)
    - Keep only GlobalState and GlobalStateKey (both used)

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -5,15 +5,5 @@ pub mod secrets;
 pub mod state_manager;
 pub mod task_storage;
 
-pub use disk::{GlobalFileNames, cleanup_orphaned_temp_files};
 pub use global_state::GlobalState;
-pub use migration::{
-    DryRunMigrationReport, ExecutedOperation, JsonObjectMigration, MigrationEngine, MigrationError,
-    MigrationExecutionReport, OperationType, TaskDirectoryMigration, TaskHistoryMigration,
-    plan_dry_run_migration,
-};
-pub use secrets::SecretsStore;
 pub use state_manager::GlobalStateKey;
-pub use state_manager::StateManager;
-pub use state_manager::{list_tasks, sort_by_timestamp, total_pages};
-pub use task_storage::TaskStorage;
